c02dc515edd24ed1d0da1f66dcec52cbb412b4c7
Sören Kuklau <chucker23n+github@gmail.com>
2021-06-18 00:51:22 +0200

    relatively naïve approach of generating output by taking the most frequent color

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+#nullable enable
+
+namespace TiffTheSeason
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputDir = new DirectoryInfo("../../../input");
+            var outputDir = new DirectoryInfo("../../../output");
+
+            outputDir.Create();
+
+            // replicate input dir hierarchy into output dir
+
+            foreach (var inputFSItem in inputDir.EnumerateFileSystemInfos("*", new EnumerationOptions
+            {
+                RecurseSubdirectories = true
+            }))
+            {
+                var relativePath = inputFSItem.GetPathRelativeTo(inputDir);
+                string equivalentOutputPath = Path.Combine(outputDir.FullName, relativePath);
+
+                switch (inputFSItem)
+                {
+                    case DirectoryInfo directory:
+                        Directory.CreateDirectory(equivalentOutputPath);
+                        break;
+                    case FileInfo inputFile:
+                        Color color;
+                        using (var inBitmap = new Bitmap(inputFile.FullName))
+                        {
+                            color = inBitmap.GetMostFrequentColor();
+                        }
+
+                        using (var outBitmap = new Bitmap(16, 16))
+                        using (var g = Graphics.FromImage(outBitmap))
+                        {
+                            g.Clear(color);
+
+                            g.Save();
+
+                            outBitmap.Save(equivalentOutputPath, ImageFormat.Tiff);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    public static class FileSystemInfoExtensions
+    {
+        public static string GetPathRelativeTo(this FileSystemInfo fileSystemInfo, FileSystemInfo baseFsi)
+        {
+            var pathUri = new Uri(fileSystemInfo.FullName);
+
+            string basePath = baseFsi.FullName;
+
+            // Folders must end in a slash
+            if (!basePath.EndsWith(Path.DirectorySeparatorChar.ToString()))
+                basePath += Path.DirectorySeparatorChar;
+
+            var folderUri = new Uri(basePath);
+
+            return Uri.UnescapeDataString(folderUri.MakeRelativeUri(pathUri)
+                                                   .ToString()
+                                                   .Replace('/', Path.DirectorySeparatorChar));
+        }
+    }
+
+    public static class BitmapExtensions
+    {
+        public static Color GetMostFrequentColor(this Bitmap bitmap)
+        {
+            var colors = new List<Color>();
+
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    colors.Add(bitmap.GetPixel(x, y));
+                }
+            }
+
+            return colors.GroupBy(c => c)
+                         .OrderByDescending(g => g.Count())
+                         .First().Key;
+        }
+    }
+}
